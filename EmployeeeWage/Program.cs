e855afc3ffafb7dfa3128c282cf015205a7c0978
Sayunkta Mankar <sayumankar77@gmail.com>
2023-06-04 15:46:48 +0530

    AddFile

---
+++ EmployeeeWage/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace EmployeeeWage
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage.Attendance();
+        }
+    }
+}
