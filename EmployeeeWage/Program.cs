30b40f6ac1c0532680ad18c2ec65645717c4e1fe
Sayunkta Mankar <sayumankar77@gmail.com>
2023-06-04 17:53:35 +0530

    AddFile

--- EmployeeeWage/Program.cs
+++ EmployeeeWage/Program.cs
@@ -12,21 +12,26 @@ namespace EmployeeeWage
             //EmployeeeWage employeeeWage = new EmployeeeWage();
             // EmpWage.TotalWage();
             // EmpWage.CalculatingPartTimeWage();
-           // EmployeeDetails.CalculatingPartTimeWageWithSwitch();
-           // EmployeeDetails.CalculateWagesForMonth();
+            // EmployeeDetails.CalculatingPartTimeWageWithSwitch();
+            // EmployeeDetails.CalculateWagesForMonth();
             //WageTillCondition.CalculateWageTillCondition();
-           // Refactor.GetWorkingHour(2);
-            MultipleCompanies.Multiple("Nokia", 10, 20, 100);
+            // Refactor.GetWorkingHour(2);
+            // MultipleCompanies.Multiple("Nokia", 10, 20, 100);
 
-            TotalWage google = new TotalWage("Google", 20, 5, 160);
-            TotalWage tata = new TotalWage("Tata", 10, 5, 160);
+            // TotalWage google = new TotalWage("Google", 20, 5, 160);
+            // TotalWage tata = new TotalWage("Tata", 10, 5, 160);
 
-            google.computeEmpWage();
-            Console.WriteLine(google.toString());
+            // google.computeEmpWage();
+            // Console.WriteLine(google.toString());
 
-            tata.computeEmpWage();
-            Console.WriteLine(tata.toString());
+            // tata.computeEmpWage();
+            // Console.WriteLine(tata.toString());
 
+
+            Building_Array emp = new Building_Array();
+            emp.addCompanyEmpWage("Audi", 20, 5, 160);
+            emp.addCompanyEmpWage("Mercedes", 50, 5, 160);
+            emp.computeEmpWage();
         }
     }
 }
